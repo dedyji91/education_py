b3cbead8aa2e93d590413525f77b05815c00fb81
darthvader <kasha8814@gmail.com>
2015-11-23 11:25:05 +0400

    d

--- less6.py
+++ less6.py
@@ -1,16 +1,18 @@
 a = float(input())
 b = float(input())
 c = input()
-if (b == 0) and (c == "mod"):
-    print("Деление на 0!")
-elif c == "*":
-    print(a * b)
-elif (b != 0) and (c == "/"):
-    print(a / b)
-elif c == "+":
+if c == "+":
     print(a + b)
 elif c == "-":
     print(a - b)
+elif (b != 0) and (c == "/"):
+    print(a / b)
+elif c == "*":
+    print(a * b)
+elif (b == 0) and (c == "mod"):
+    print("Деление на 0!")
+elif (b != 0) and (c == "mod"):
+    print(a % b)
 elif c == "pow":
     print(a ** b)
 elif (b != 0) and (c == "div"):
